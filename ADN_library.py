5831116714f73770304f309f2c5044da5a659767
Portglass <obri.origine@gmail.com>
2023-04-13 12:59:44 +0200

    fasta read and new functions in ADN library

--- ADN_library.py
+++ ADN_library.py
@@ -2,7 +2,16 @@ import sys, re
 
 def read_file(path):
     f=open(path,'r')
-    print(f.readlines())
-    print("okay")
+    header = f.readline()
+    ADN =""
+    for line in f:
+        ADN += line[0:-1]
+    ADN = list(ADN)
+    return header, ADN
 
-read_file('Data/sequence.fasta')
+def nbNucleo(ADN):
+    return ADN.count("A"), ADN.count("T"), ADN.count("C"), ADN.count("G")
+
+def pourcentNucleo(ADN):
+    a,t,c,g = nbNucleo(ADN)
+    return a/len(ADN), a/len(ADN), a/len(ADN), a/len(ADN)
