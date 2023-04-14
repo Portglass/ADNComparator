c37d22743f7c2134f6f9397f031780c11ba7f64a
Portglass <obri.origine@gmail.com>
2023-04-14 11:08:48 +0200

    Upload fasta file

--- Main.py
+++ Main.py
@@ -1,16 +1,11 @@
 import ADN_library as al
-import streamlit as st
 import Visual_Streamlit as vs
 import streamlit as st
-import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("Agg")
-from Bio.Seq import Seq
 from Bio import SeqIO
-from collections import Counter
-import neatbio.sequtils as utils
-import numpy as np
-from PIL import Image
+from io import StringIO
+import altair as alt
 
 
 
@@ -24,33 +19,31 @@ def main():
         st.subheader("DNA details")
         seq_file = st.file_uploader("Upload FASTA File", type=["fasta", "fa"])
         if seq_file is not None:
-                dna_record = SeqIO.read(seq_file, "fasta")
-                # st.write(dna_record)
-                dna_seq = dna_record.seq
+            stringio = StringIO(seq_file.getvalue().decode("utf-8"))
+            sequence = ""
+            for record in SeqIO.parse(stringio, 'fasta'):
+                sequence = str(record.seq)
+                print('Passed')
+                st.write(f'Length of sequence: {len(sequence)}')
 
-                details = st.radio("Details", ("Description", "Sequence"))
-                if details == "Description":
-                    st.write(dna_record.description)
-                elif details == "Sequence":
-                    st.write(dna_record.seq)
+            df_ADN = al.fromStringToDataframe(list(sequence))
+
+            vs.bar_chart(df_ADN)
+            _, df_pourcent_ADN = al.pourcentNucleo(list(sequence))
+            bar_chart = alt.Chart(df_pourcent_ADN).mark_bar().encode(
+                y='Pourcent:Q',
+                x='Nucleotide:O',
+            )
+
+            st.altair_chart(bar_chart, use_container_width=True)
+
+
 
     elif choice == 'About':
         st.subheader("About")
 
 
-        """
-        header, ADN_list = al.read_file(uploaded_file)
-        df_ADN = al.fromFileToDataframe(uploaded_file)
 
-        vs.bar_chart(df_ADN)
-        _, df_pourcent_ADN = al.pourcentNucleo(ADN_list)
-        bar_chart = alt.Chart(df_pourcent_ADN).mark_bar().encode(
-            y='Pourcent:Q',
-            x='Nucleotide:O',
-        )
-
-        st.altair_chart(bar_chart, use_container_width=True)
-        """
 
 if __name__ == '__main__':
 	main()
